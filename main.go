c7fc8982674401860902a9670bf6194abf5f8eef
Ramish Hassan <ramishshah93@gmail.com>
2022-09-13 15:36:23 +0200

    interface struct and a couple of func for persons info ready

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import "pluralsight/go-person/organization"
+
+
+
 func main() {
-	
+	p:= organization.NewPerson(
+		firstName: "ramish", lastName: "hassae"
+	)
+	println(p.ID())
+	println(p.FullName())
 }
--- organization/person.go
+++ organization/person.go
@@ -1,1 +1,27 @@
 package organization
+
+import "fmt"
+
+type Identifiable interface{
+	ID() 
+}
+
+type Person struct {
+	firstName string
+	lastName string
+}
+
+func NewPerson(firstName, lastName string) Person{
+	return Person{
+		firstName: firstName,
+		lastName: lastName,
+	}
+}
+
+func (p Person) FullName() string{
+	return fmt.Sprintf("%s %s", p.firstName, p.lastName)
+}
+
+func (p Person) ID() string {
+	return "1234"
+}
