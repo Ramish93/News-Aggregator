bdf4352a345e0a4783cafe289415463340b157e7
Ramish Hassan <ramishshah93@gmail.com>
2022-09-15 10:12:49 +0200

    fetching xml file and parsing it into variables

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/xml"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 )
 
@@ -9,8 +11,27 @@ func index_handler(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "go is neat")
 }
 
+type SitemapIndex struct {
+	Locations []Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+
 func main() {
-	http.HandleFunc("/", index_handler)
-	fmt.Println("server going live on port 3000")
-	http.ListenAndServe(":3000", nil)
+	resp, _ := http.Get("https://www.washingtonpost.com/wp-stat/sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	
+	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
+
+	// http.HandleFunc("/", index_handler)
+	// fmt.Println("server going live on port 3000")
+	// http.ListenAndServe(":3000", nil)
 }
