fde0527537ce5880557b93b762f4ef76ab5f1912
Ramish Hassan <ramishshah93@gmail.com>
2022-09-15 22:44:43 +0200

    template.html file complete, next passing the api call to go routine to decrease page loading time

--- main.go
+++ main.go
@@ -3,18 +3,22 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
-
 	"html/template"
 	"io/ioutil"
 	"net/http"
 )
 
-func index_handler(w http.ResponseWriter, r *http.Request){
-	fmt.Fprintf(w, "go is neat")
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
+type NewsAggPage struct {
+    Title string
+    News map[string]NewsMap
+}
 
-type SitemapIndex struct {
+type Sitemapindex struct {
 	Locations []string `xml:"sitemap>loc"`
 }
 
@@ -24,47 +28,36 @@ type News struct {
 	Locations []string `xml:"url>loc"`
 }
 
-type NewsMap struct {
-	Keyword string
-	Location string
-}
-type NewsAggPage struct {
-	Title string
-	News map[string]NewsMap
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Whoa, Go is neat!</h1>")
 }
 
-func newsAggHandler(w http.ResponseWriter, r *http.Request){
-	var s SitemapIndex
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	var s Sitemapindex
 	var n News
-	news_map := make(map[string] NewsMap)
-
 	resp, _ := http.Get("https://www.washingtonpost.com/wp-stat/sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	
-	resp.Body.Close()
-
 	xml.Unmarshal(bytes, &s)
+	news_map := make(map[string]NewsMap)
 
-	for _, Location := range s.Locations{
+	for _, Location := range s.Locations {
 		resp, _ := http.Get(Location)
+		fmt.Println("Location: ", Location)
 		bytes, _ := ioutil.ReadAll(resp.Body)
-		
-		resp.Body.Close()
 		xml.Unmarshal(bytes, &n)
-		fmt.Println(n)
-		for idx,_ := range n.Titles {
-			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
 		
+		for idx, _ := range n.Keywords {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
 		}
 	}
 
-	p:= NewsAggPage{Title: "abc", News: news_map}
-	t, _ := template.ParseFiles("template.html")
-
-	fmt.Println(t.Execute(w, p))
+	p := NewsAggPage{Title: "Amazing News Aggregator", News: news_map}
+    t, _ := template.ParseFiles("newsaggtemplate.html")
+    fmt.Println(t.Execute(w, p))
 }
 
 func main() {
-
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/agg/", newsAggHandler)
+	http.ListenAndServe(":8000", nil) 
 }
-
