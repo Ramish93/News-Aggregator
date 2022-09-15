80b6424e8b05ab755c82ba879dbd797833a044d1
Ramish Hassan <ramishshah93@gmail.com>
2022-09-15 14:03:03 +0200

    fixed typo that was causing titles to not pupulate(typed caps Kyeword in News struct)

--- main.go
+++ main.go
@@ -17,26 +17,43 @@ type SitemapIndex struct {
 
 type News struct {
 	Titles []string `xml:"url>news>title"`
-	Keywords []string `xml:"url>news>Keywords"`
+	Keywords []string `xml:"url>news>keywords"`
 	Locations []string `xml:"url>loc"`
+}
+
+type NewsMap struct {
+	Keyword string
+	Location string
 }
 
 
 func main() {
+	var s SitemapIndex
+	var n News
+	news_map := make(map[string] NewsMap)
+
 	resp, _ := http.Get("https://www.washingtonpost.com/wp-stat/sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
 	
 	resp.Body.Close()
 
-	var s SitemapIndex
 	xml.Unmarshal(bytes, &s)
-	// fmt.Println(s.Locations)
 
 	for _, Location := range s.Locations{
-		fmt.Printf("\n %s", Location)
+		resp, _ := http.Get(Location)
+		bytes, _ := ioutil.ReadAll(resp.Body)
+		
+		resp.Body.Close()
+		xml.Unmarshal(bytes, &n)
+		fmt.Println(n)
+		for idx,_ := range n.Titles {
+			news_map[n.Titles[idx]] = NewsMap{n.Keywords[idx], n.Locations[idx]}
+		
+		}
 	}
 
-	// http.HandleFunc("/", index_handler)
-	// fmt.Println("server going live on port 3000")
-	// http.ListenAndServe(":3000", nil)
 }
+
+// http.HandleFunc("/", index_handler)
+// fmt.Println("server going live on port 3000")
+// http.ListenAndServe(":3000", nil)
